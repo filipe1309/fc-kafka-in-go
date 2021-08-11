44377a07953125edd45fe7a45de6a6feb0754282
Filipe Leuch Bonfim <filipe1309@gmail.com>
2021-08-10 23:49:50 -0300

    feat: init kafka producer dev

---
+++ cmd/producer/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/confluentinc/confluent-kafka-go/kafka"
+)
+
+func main() {
+	fmt.Println("Hello Go")
+}
+
+func NewKafkaProducer() *kafka.Producer {
+	configMap := &kafka.ConfigMap{
+		"bootstrap.servers": "kafka_kafka_1:9092",
+	}
+
+	p, err := kafka.NewProducer(configMap)
+	if err != nil {
+		log.Println(err.Error())
+	}
+
+	return p
+}
