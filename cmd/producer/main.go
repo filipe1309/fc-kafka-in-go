4175a0e56fee00a47c57bada9b16ae0afdd9892b
Filipe Leuch Bonfim <filipe1309@gmail.com>
2021-08-11 16:53:56 -0300

    feat: gen a delivery report with delivery channel

--- cmd/producer/main.go
+++ cmd/producer/main.go
@@ -1,14 +1,25 @@
 package main
 
 import (
+	"fmt"
 	"log"
 
 	"github.com/confluentinc/confluent-kafka-go/kafka"
 )
 
 func main() {
+	deliveryChannel := make(chan kafka.Event)
 	producer := NewKafkaProducer()
-	Publish("hello", "TESTE", producer, nil)
+	Publish("hello", "TESTE", producer, nil, deliveryChannel)
+
+	e := <-deliveryChannel
+	msg := e.(*kafka.Message)
+	if msg.TopicPartition.Error != nil {
+		fmt.Println("ERROR on sent:", msg.TopicPartition.Error)
+	} else {
+		fmt.Println("Message sent:", msg.TopicPartition)
+	}
+
 	producer.Flush(1 * 1000)
 }
 
@@ -25,14 +36,14 @@ func NewKafkaProducer() *kafka.Producer {
 	return p
 }
 
-func Publish(msg string, topic string, producer *kafka.Producer, key []byte) error {
+func Publish(msg string, topic string, producer *kafka.Producer, key []byte, deliveryChan chan kafka.Event) error {
 	message := &kafka.Message{
 		Value:          []byte(msg),
 		Key:            key,
 		TopicPartition: kafka.TopicPartition{Topic: &topic, Partition: kafka.PartitionAny},
 	}
 
-	err := producer.Produce(message, nil)
+	err := producer.Produce(message, deliveryChan)
 	if err != nil {
 		return err
 	}
